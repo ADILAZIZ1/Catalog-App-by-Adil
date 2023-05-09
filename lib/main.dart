bfebe082af96f7ddce4bd1d0cd9fa6b1e9712789
ADILAZIZ1 <114533338+ADILAZIZ1@users.noreply.github.com>
2023-05-10 00:55:00 +0500

    day4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:practice_2/pages/home_page.dart';
 import 'package:practice_2/pages/login_page.dart';
 
@@ -14,7 +15,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.amber),
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
+      darkTheme: ThemeData(brightness: Brightness.dark),
       routes: {
         "/": (context) => LoginPage(),
         "/login": (context) => LoginPage(),
